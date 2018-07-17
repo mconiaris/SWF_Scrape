05b779e3af90d21a98e332dea563752c2449d030
mconiaris <mconiaris@gmail.com>
2018-07-17 10:50:00 -0400

    Blanked out things that I don't need right now.

--- main.rb
+++ main.rb
@@ -2,14 +2,15 @@ require 'pdf-reader'
 
 reader = PDF::Reader.new("files/Zak Knight.pdf")
 
-puts reader.pdf_version
-puts reader.info
-puts reader.metadata
-puts reader.page_count
+# puts reader.pdf_version
+# puts reader.info
+# puts reader.metadata
+# puts reader.page_count
 
 
 reader.pages.each do |page|
-   puts page.fonts
+   # puts page.fonts
    puts page.text
    # puts page.raw_content
  end
+
