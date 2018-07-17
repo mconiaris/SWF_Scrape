82d47957c6fc1c5c002c41f57c1f0be1b01cf5d5
mconiaris <mconiaris@gmail.com>
2018-07-17 10:44:35 -0400

    Added template page reading code.

--- main.rb
+++ main.rb
@@ -6,3 +6,10 @@ puts reader.pdf_version
 puts reader.info
 puts reader.metadata
 puts reader.page_count
+
+
+reader.pages.each do |page|
+   puts page.fonts
+   puts page.text
+   # puts page.raw_content
+ end
