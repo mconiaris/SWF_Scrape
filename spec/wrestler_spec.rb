b701af3b0b341e8675325cc2ef9b654eaac52da8
mconiaris <mconiaris@gmail.com>
2018-08-08 14:27:08 -0400

    Created an rspec test that confirms initialization of a Wrestler class.

--- spec/wrestler_spec.rb
+++ spec/wrestler_spec.rb
@@ -1,3 +1,15 @@
+require 'wrestler'
+
 # Main Spec
 describe Wrestler do
+	describe 'can be initialized' do
+		it 'with no name' do
+			wrestler = Wrestler.new
+			expect(wrestler.get_name).to eql(nil)
+		end
+	end
+end
+
+describe 'PDF Reader' do
+
 end
