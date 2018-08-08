e7f63b405c85a0576a6b19aa70b71fde73501f73
mconiaris <mconiaris@gmail.com>
2018-08-08 15:00:03 -0400

    Created more tests. The last one is incomplete.

--- spec/wrestler_spec.rb
+++ spec/wrestler_spec.rb
@@ -12,9 +12,18 @@ describe Wrestler do
 	end
 end
 
+# TODO: Fix structure of rspec test so that one test builds
+# on another.
 describe 'PDF Reader' do
 	it 'loads the Zak Knight PDF as a Reader file' do
 		reader = PDF::Reader.new("files/Zak Knight.pdf")
 		expect(reader.class).to eql(PDF::Reader)
+	end
+	describe 'Zak Knight file' do
+		it 'reader.pages[0].text returns text as a String' do
+			wrestler_text = reader.pages[0].text
+			expect(wrestler_text.class).to eql(String)
+		binding.pry
+		end
 	end
 end
