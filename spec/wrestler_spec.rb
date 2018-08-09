c5db8ed9cf2399c68f7f9fdb10498b32712969c7
mconiaris <mconiaris@gmail.com>
2018-08-09 13:51:02 -0400

    Fixed RSpec issue. Require statement was flawed.

--- spec/wrestler_spec.rb
+++ spec/wrestler_spec.rb
@@ -1,16 +1,20 @@
 require 'pdf-reader'
 require 'pry'
-require 'wrestler'
+require_relative 'wrestler'
 
 # Main Spec
 RSpec.describe Wrestler do
 	before(:example) do
 		wrestler = Wrestler.new
+		puts 'before block executes.'
 	end
 
-	context 'initialize' do
+	describe '#initialize' do
 		it 'creates an object of it\'s class' do
 			expect(described_class).to eq(Wrestler)
+		end
+		it 'has a name value of nil' do
+			expect(wrestler.get_name).to eq(nil)
 		end
 	end
 end
