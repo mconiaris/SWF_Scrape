318beda66e4c214364a6fbdfbf52e4da92792a44
mconiaris <mconiaris@gmail.com>
2018-08-09 11:46:09 -0400

    Added an rspec test.

--- spec/wrestler_spec.rb
+++ spec/wrestler_spec.rb
@@ -5,6 +5,11 @@ require 'wrestler'
 # Main Spec
 describe Wrestler do
 	describe 'can be initialized' do
+		it 'as a Wrestler object' do
+			wrestler = Wrestler.new
+			expect(wrestler.class).to eql(Wrestler)
+		end
+
 		it 'with no name' do
 			wrestler = Wrestler.new
 			expect(wrestler.get_name).to eql(nil)
