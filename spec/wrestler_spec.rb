c070cf7874f2f19442ef5022ecfbd39a5f32b1ec
mconiaris <mconiaris@gmail.com>
2018-08-14 14:54:09 -0400

    Revised rspect tests for readability.

--- spec/wrestler_spec.rb
+++ spec/wrestler_spec.rb
@@ -13,26 +13,26 @@ RSpec.describe Wrestler do
 		it 'creates an object of it\'s class' do
 			expect(described_class).to eq(Wrestler)
 		end
-		it 'has a name value of nil' do
+		it 'that has a name value of nil' do
 			expect(@wrestler.get_name).to eq(nil)
 		end
-		it 'has a get_name method' do
+		it 'that has a get_name method' do
 			expect(@wrestler.respond_to?(:get_name)).to eq(true)
 		end
-		it 'has a set_name method' do
+		it 'that has a set_name method' do
 			expect(@wrestler.respond_to?(:set_name)).to eq(true)
 		end
-		it 'has a set_file_name method' do
+		it 'that has a set_file_name method' do
 			expect(@wrestler.respond_to?(:set_file_name)).to eq(true)
 		end
-		it 'has a process_offense method' do
+		it 'that has a process_offense method' do
 			expect(@wrestler.respond_to?(:process_offense)).to eq(true)
 		end
-		it 'has a process_defense method' do
+		it 'that has a process_defense method' do
 			expect(@wrestler.respond_to?(:process_defense)).to eq(true)
 		end
 		# process_text
-		it 'has a process_text method' do
+		it 'that has a process_text method' do
 			expect(@wrestler.respond_to?(:process_text)).to eq(true)
 		end
 	end
