77c2cd3f6c226810a12779f8c5d0b931c018dc40
mconiaris <mconiaris@gmail.com>
2018-10-17 14:27:51 -0400

    Created blank scraper.rb file.

--- main.rb
+++ main.rb
@@ -1,5 +1,10 @@
+require 'pdf-reader'
 require 'pry'
 require_relative 'wrestler'
 
-#Create Wrestler object
+# Create Wrestler object
 @wrestler = Wrestler.new
+
+# Scrape PDF
+reader = PDF::Reader.new("files/Zak Knight.pdf")
+binding.pry
