3aa5d8a272af035ba252e20c9f13bc0df263e3d3
mconiaris <mconiaris@gmail.com>
2018-10-17 14:58:55 -0400

    Added TODOs.

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
 require 'pdf-reader'
 require 'pry'
+require 'scraper'
 require_relative 'wrestler'
 
 # Create Wrestler object
 @wrestler = Wrestler.new
 
+#TODO: Moves this to scraper.rb and call it from here.
 # Scrape PDF
 reader = PDF::Reader.new("files/Zak Knight.pdf")
 binding.pry
--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,5 @@
 require 'pdf-reader'
 
 class Scraper
+	#TODO: Factor out -pdf read here to that a file can be passed to it.
 end
