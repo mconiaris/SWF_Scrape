2f29570988c287835b897dd3a4e1a9e34cf81499
mconiaris <mconiaris@gmail.com>
2018-10-17 14:55:50 -0400

    Created a page count test for scraper.

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,11 +1,13 @@
 RSpec.describe Scraper do
-	before(:contaxt) do
+	before(:context) do
 		@reader = PDF::Reader.new("files/Zak Knight.pdf")
 	end
 	
-	describe "Zak Knight PDF" do
-		it "should have one page" do
-			exect (@reader.page_count).to eq(1)
+	describe 'Scraper' do
+		context 'Zak Knight PDF' do
+			it "should have one page" do
+				expect(@reader.page_count).to eq(1)
+			end
 		end
 	end
 end
