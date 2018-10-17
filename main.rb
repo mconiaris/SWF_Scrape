92f7efb47a16d30132b7363ff9ac22fd50cb93d3
mconiaris <mconiaris@gmail.com>
2018-10-17 12:41:00 -0400

    Starting over with main.rb.

--- main.rb
+++ main.rb
@@ -3,7 +3,3 @@ require_relative 'wrestler'
 
 #Create Wrestler object
 @wrestler = Wrestler.new
-wrestler_text = @wrestler.process_source("files/Zak Knight.pdf")
-
-# puts wrestler_text
-@wrestler.process_text(wrestler_text)
