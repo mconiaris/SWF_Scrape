23fbbb54294d43e59c9fed9142cc31ca071b2c76
mconiaris <mconiaris@gmail.com>
2018-10-18 14:04:29 -0400

    Created additional tests to confirm that right PDF was loaded.

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Scraper do
 			it "should have one page" do
 				expect(@scrape.reader.page_count).to eq(1)
 			end
+			it "should have 1469 characters" do
+				expect(@scrape.reader.page(1).text.size).to eq(1469)
+			end
+			it "should include 'Zak Knight' in the text." do
+				expect(@scrape.reader.page(1).text).to include("Zak Knight")
+			end
 		end
 	end
 end
