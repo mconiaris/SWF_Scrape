44cda5cad97918d6252a966fff4d32cfe28110b8
mconiaris <mconiaris@gmail.com>
2018-11-05 11:06:57 -0500

    Revised rspec test.

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -19,8 +19,7 @@ RSpec.describe Scraper do
 			# TODO: Factor this test out to test caputre_text
 			# TODO: Perhaps create it's own block with it's own initialize
 			it 'should have Zak Knight in the first line' do
-				@card = @scrape.reader.page(1).text
-				expect(@card.include?('Zak Knight')).to eq(true)
+				expect(@scrape.card.include?('Zak Knight')).to eq(true)
 			end
 		end
 	end
