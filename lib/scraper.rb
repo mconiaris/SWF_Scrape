272e4242708539c0f2f6f8353eebff92cfebe0a5
mconiaris <mconiaris@gmail.com>
2018-11-08 14:15:28 -0500

    Added DC and Specialty text to hash.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,15 +35,48 @@ class Scraper
 		}
 		# Create Hash for card and add vales.
 		card_hash = Hash.new
+
+		# Add card name to hash
 		card_hash[:name] = left[0]
 
-		# Strip out empty spaces and redundant OC & DC text
+		# Strip out empty spaces and redundant OC text
 		card_hash[:OC02] = left[2][0..15].strip.split[1]
 		card_hash[:OC03] = left[3][0..15].strip.split[1]
-
+		card_hash[:OC04] = left[4][0..15].strip.split[1]
+		card_hash[:OC05] = left[5][0..15].strip.split[1]
+		card_hash[:OC06] = left[6][0..15].strip.split[1]
 		card_hash[:OC07] = left[2][16..left.length].strip.split[1]
 		card_hash[:OC08] = left[3][16..left.length].strip.split[1]
-		# binding.pry
+		card_hash[:OC09] = left[4][16..left.length].strip.split[1]
+		card_hash[:OC10] = left[5][16..left.length].strip.split[1]
+		card_hash[:OC11] = left[6][16..left.length].strip.split[1]
+
+		# Add OC12 to hash
+		card_hash[:OC12] = left[7].strip.split[1]
+
+		# Strip DC redundant text and put values into the hash
+		card_hash[:DC02] = left[9][0..15].strip.split[1]
+		card_hash[:DC03] = left[10][0..15].strip.split[1]
+		card_hash[:DC04] = left[11][0..15].strip.split[1]
+		card_hash[:DC05] = left[12][0..15].strip.split[1]
+		card_hash[:DC06] = left[13][0..15].strip.split[1]
+		card_hash[:DC07] = left[9][16..left.length].strip.split[1]
+		card_hash[:DC08] = left[10][16..left.length].strip.split[1]
+		card_hash[:DC09] = left[11][16..left.length].strip.split[1]
+		card_hash[:DC10] = left[12][16..left.length].strip.split[1]
+		card_hash[:DC11] = left[13][16..left.length].strip.split[1]
+		card_hash[:DC12] = left[14].split[1]
+
+		# Add Specialty Values to hash
+		card_hash[:Specialty] = left[16]
+		card_hash[:S1] = left[17].split(/\d\s(.+)/)[1]
+		card_hash[:S2] = left[18].split(/\d\s(.+)/)[1]
+		card_hash[:S3] = left[19].split(/\d\s(.+)/)[1]
+		card_hash[:S4] = left[20].split(/\d\s(.+)/)[1]
+		card_hash[:S5] = left[21].split(/\d\s(.+)/)[1]
+		card_hash[:S6] = left[22].split(/\d\s(.+)/)[1]
+
+		binding.pry
 		# TODO: Divide left array GC & DC lines in half
 		# TODO: Convert arrays into one hash
 		# TODO: Create Wrestler object
