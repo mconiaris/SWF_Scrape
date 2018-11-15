aceed2797398cb3f4206b483611fc99ed7e98419
mconiaris <mconiaris@gmail.com>
2018-11-15 09:51:47 -0500

    Reorganized main.rb. Fixed a few errors and added better notes.

--- main.rb
+++ main.rb
@@ -6,12 +6,15 @@ require_relative 'lib/wrestler'
 
 # Scrape PDF
 @scrape = Scraper.new("files/Zak Knight.pdf")
+
+# Isolate card moves array
 card = @scrape.card
-@scrape.process_card(card)
-binding.pry
-# binding.pry
+
+# Convert move array into a hash and fix formatting.
+moves = @scrape.process_card(card)
 
 # Create Wrestler object
-@wrestler = Wrestler.new
+@wrestler = Wrestler.new(moves)
+binding.pry
 
 # TODO Add functionality to select files from computer.
