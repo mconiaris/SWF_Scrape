9e2b5cd9480299c178f22cab2aab536b347e1980
mconiaris <mconiaris@gmail.com>
2018-11-19 11:38:52 -0500

    Made rspec test for Analyzer.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -1,2 +1,11 @@
 RSpec.describe Analyzer do
+	before(:context) do
+		@analyze = Analyzer.new
+	end
+
+	describe '#analyze_gc' do
+		it "should respond_to analyze_gc" do
+			expect(@analyze.respond_to?(:analyze_gc))
+		end
+	end
 end
