8748a3a004a93399066b490c54c53ab7a5fd402d
mconiaris <mconiaris@gmail.com>
2018-11-28 11:00:43 -0500

    Commented out most methods. Time to refactor and create tests.

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -66,142 +66,184 @@ class Analyzer
 				end
 		}
 
-		# Calculate OC points
-		oc_points = calculate_oc_ropes_points(oc_points_array, gc_oc_roll)
-		oc_prob_hash = calculate_pa_ropes_sub_xx_dq(oc_points_array)
-binding.pry
-		# Multiply ropes values by oc_ropes_probability
-		# This is to calculate a X-per-roll.
-		oc_ropes_probability = oc_prob_hash[:ropes]
-		ropes_points = calculate_oc_ropes_points(ropes_points_array)
-		ropes_points = ropes_points * oc_ropes_probability
-		ropes_prob_hash = calculate_pa_ropes_sub_xx_dq(ropes_points_array)
-		w[:oc_probability] = gc_oc_roll
-		w[:dc_probability] = 1 - gc_oc_roll
-		w[:dc_points_per_roll] = dc_points * w[:dc_probability].to_f
-		p_a = sum_of_s_array(s_stats_array)
-		w[:s_pin_attempt_count] = p_a[:s_pin_attempt_count]
-		w[:s_points] = p_a[:s_points]
-		w[:sub_probability] = sub_tag_probability(wrestler.values[:Sub1], wrestler.values[:Sub2])
-		w[:tag_probability] = sub_tag_probability(wrestler.values[:Tag1], wrestler.values[:Tag2])
-		w[:singles_priority] = wrestler.values[:PriorityS]
-		w[:tag_priority] = wrestler.values[:PriorityT]
+# 		# Calculate OC points
+# 		oc_points = calculate_oc_ropes_points(oc_points_array, gc_oc_roll)
+# 		oc_prob_hash = calculate_pa_ropes_sub_xx_dq(oc_points_array, gc_oc_roll)
+# 		# Multiply ropes values by oc_ropes_probability
+# 		# This is to calculate a X-per-roll.
+# 		oc_ropes_probability = oc_prob_hash[:ropes]
+# 		ropes_points = calculate_oc_ropes_points(ropes_points_array)
+# 		ropes_points = ropes_points * oc_ropes_probability
+# 		ropes_prob_hash = calculate_pa_ropes_sub_xx_dq(ropes_points_array)
+# 		w[:oc_probability] = gc_oc_roll
+# 		w[:dc_probability] = 1 - gc_oc_roll
+# 		w[:dc_points_per_roll] = dc_points * w[:dc_probability].to_f
+# 		p_a = sum_of_s_array(s_stats_array)
+# 		w[:s_pin_attempt_count] = p_a[:s_pin_attempt_count]
+# 		w[:s_points] = p_a[:s_points]
+# 		w[:sub_probability] = sub_tag_probability(wrestler.values[:Sub1], wrestler.values[:Sub2])
+# 		w[:tag_probability] = sub_tag_probability(wrestler.values[:Tag1], wrestler.values[:Tag2])
+# 		w[:singles_priority] = wrestler.values[:PriorityS]
+# 		w[:tag_priority] = wrestler.values[:PriorityT]
 
-binding.pry
+# binding.pry
 		return w
 	end
 
-	def analyze_gc(k, v)
-		if v.include?('OC')
-			case k
-			when :GC02
-				return TWO_TWELVE
-			when :GC03
-				return THREE_ELEVEN
-			when :GC04
-				return FOUR_TEN
-			when :GC05
-				return FIVE_NINE
-			when :GC06
-				return SIX_EIGHT
-			when :GC07
-				return SEVEN
-			when :GC08
-				return SIX_EIGHT
-			when :GC09
-				return FIVE_NINE
-			when :GC10
-				return FOUR_TEN
-			when :GC11
-				return THREE_ELEVEN
-			when :GC12
-				return TWO_TWELVE
-			else
-				return 0
-			end
-		else
-			return 0
-		end
-	end
 
-	
-	def analyze_dc(k, v)
+	def calculate_probability(key)
+		k = key[2..3]
+
 		case k
-		when :DC02
-			return dc_point_roll(TWO_TWELVE, v)
-		when :DC03
-			dc_point_roll(THREE_ELEVEN, v)
-		when :DC04
-			return dc_point_roll(FOUR_TEN, v)
-		when :DC05
-			return dc_point_roll(FIVE_NINE, v)
-		when :DC06
-			return dc_point_roll(SIX_EIGHT, v)
-		when :DC07
-			return dc_point_roll(SEVEN, v)
-		when :DC08
-			return dc_point_roll(SIX_EIGHT, v)
-		when :DC09
-			return dc_point_roll(FIVE_NINE, v)
-		when :DC10
-			return dc_point_roll(FOUR_TEN, v)
-		when :DC11
-			return dc_point_roll(THREE_ELEVEN, v)
-		when :DC12
-			return dc_point_roll(TWO_TWELVE, v)
+		when '02'
+			return TWO_TWELVE
+		when '03'
+			return THREE_ELEVEN
+		when '04'
+			return FOUR_TEN
+		when '05'
+			return FIVE_NINE
+		when '06'
+			return SIX_EIGHT
+		when '07'
+			return SEVEN
+		when '08'
+			return SIX_EIGHT
+		when '09'
+			return FIVE_NINE
+		when '10'
+			return FOUR_TEN
+		when '11'
+			return THREE_ELEVEN
+		when '12'
+			return TWO_TWELVE
 		else
 			return 0
 		end
 	else
 		return 0
 	end
+	
 
 
-	# For Specialty math
-	def analyze_s(k, v)
-		v.split
-	end
 
 
-	# For Specialty math
-	def sum_of_s_array(s_array)
-
-		# TODO: Figure out DQ rolls in S.
-		# Calculate number of points
-		b = s_array.map {
-			|x| x[0].to_i
-		}
-
-		# Count number of P/A or sub attempts
-		a = s_array.map { 
-			|x| x[1] 
-		}
-		a.delete_if { 
-			|x| x == nil 
-		}
-		s_stats = Hash.new
-		s_stats[:s_pin_attempt_count] = a.size
-		s_stats[:s_points] = b.sum
-		return s_stats
-	end
 
 
-	# For DC Math
-	def dc_point_roll(roll, result)
-		if result == 'A'
-			# puts result
-			DC_A * roll
-		elsif result == 'B'
-			# puts result
-			return 0
-		elsif result == 'C'
-			# puts result
-			DC_C * roll
-		elsif result == "REVERSE"
-			# puts result
-			DC_R * roll
-		end
-	end
+	
+
+	# def analyze_gc(k, v)
+	# 	if v.include?('OC')
+	# 		case k
+	# 		when :GC02
+	# 			return TWO_TWELVE
+	# 		when :GC03
+	# 			return THREE_ELEVEN
+	# 		when :GC04
+	# 			return FOUR_TEN
+	# 		when :GC05
+	# 			return FIVE_NINE
+	# 		when :GC06
+	# 			return SIX_EIGHT
+	# 		when :GC07
+	# 			return SEVEN
+	# 		when :GC08
+	# 			return SIX_EIGHT
+	# 		when :GC09
+	# 			return FIVE_NINE
+	# 		when :GC10
+	# 			return FOUR_TEN
+	# 		when :GC11
+	# 			return THREE_ELEVEN
+	# 		when :GC12
+	# 			return TWO_TWELVE
+	# 		else
+	# 			return 0
+	# 		end
+	# 	else
+	# 		return 0
+	# 	end
+	# end
+
+	
+	# def analyze_dc(k, v)
+	# 	case k
+	# 	when :DC02
+	# 		return dc_point_roll(TWO_TWELVE, v)
+	# 	when :DC03
+	# 		dc_point_roll(THREE_ELEVEN, v)
+	# 	when :DC04
+	# 		return dc_point_roll(FOUR_TEN, v)
+	# 	when :DC05
+	# 		return dc_point_roll(FIVE_NINE, v)
+	# 	when :DC06
+	# 		return dc_point_roll(SIX_EIGHT, v)
+	# 	when :DC07
+	# 		return dc_point_roll(SEVEN, v)
+	# 	when :DC08
+	# 		return dc_point_roll(SIX_EIGHT, v)
+	# 	when :DC09
+	# 		return dc_point_roll(FIVE_NINE, v)
+	# 	when :DC10
+	# 		return dc_point_roll(FOUR_TEN, v)
+	# 	when :DC11
+	# 		return dc_point_roll(THREE_ELEVEN, v)
+	# 	when :DC12
+	# 		return dc_point_roll(TWO_TWELVE, v)
+	# 	else
+	# 		return 0
+	# 	end
+	# else
+	# 	return 0
+	# end
+
+
+# 	# For Specialty math
+# 	def analyze_s(k, v)
+# 		v.split
+# binding.pry
+# 	end
+
+
+	# # For Specialty math
+	# def sum_of_s_array(s_array)
+
+	# 	# TODO: Figure out DQ rolls in S.
+	# 	# Calculate number of points
+	# 	b = s_array.map {
+	# 		|x| x[0].to_i
+	# 	}
+
+	# 	# Count number of P/A or sub attempts
+	# 	a = s_array.map { 
+	# 		|x| x[1] 
+	# 	}
+	# 	a.delete_if { 
+	# 		|x| x == nil 
+	# 	}
+	# 	s_stats = Hash.new
+	# 	s_stats[:s_pin_attempt_count] = a.size
+	# 	s_stats[:s_points] = b.sum
+	# 	return s_stats
+	# end
+
+
+	# # For DC Math
+	# def dc_point_roll(roll, result)
+	# 	if result == 'A'
+	# 		# puts result
+	# 		DC_A * roll
+	# 	elsif result == 'B'
+	# 		# puts result
+	# 		return 0
+	# 	elsif result == 'C'
+	# 		# puts result
+	# 		DC_C * roll
+	# 	elsif result == "REVERSE"
+	# 		# puts result
+	# 		DC_R * roll
+	# 	end
+	# end
 
 	# Takes SUB or TAG values and calculates probability 
 	# a card rolling that range.
@@ -239,105 +281,74 @@ binding.pry
 			return num_range
 	end
 
-	def calculate_probability(key)
-		k = key[2..3]
 
-		case k
-		when '02'
-			return TWO_TWELVE
-		when '03'
-			return THREE_ELEVEN
-		when '04'
-			return FOUR_TEN
-		when '05'
-			return FIVE_NINE
-		when '06'
-			return SIX_EIGHT
-		when '07'
-			return SEVEN
-		when '08'
-			return SIX_EIGHT
-		when '09'
-			return FIVE_NINE
-		when '10'
-			return FOUR_TEN
-		when '11'
-			return THREE_ELEVEN
-		when '12'
-			return TWO_TWELVE
-		else
-			return 0
-		end
-	else
-		return 0
-	end
+	# # Takes move string, splits it, and then adds
+	# # points, XX, *, P/A, DQ and probability into 
+	# # an array for analysis.
+	# def create_oc_ropes_moves_array(key, move)
+	# 	m = move.split
+	# 	puts m
 
-	# Takes move string, splits it, and then adds
-	# points, XX, *, P/A, DQ and probability into 
-	# an array for analysis.
-	def create_oc_ropes_moves_array(key, move)
-		m = move.split
-		puts m
+	# 	if (m.last != '*') && (m.last != '(XX)') && m.last != ('P/A') && m.last != ('(DQ)')
+	# 		x = [calculate_probability(key), m.last]
+	# 	elsif m.last == '(S)'
+	# 		x = [calculate_probability(key), m.last]
+	# 	elsif move == 'ROPES'
+	# 		x = [calculate_probability(key), move]
+	# 	else
+	# 		x = [calculate_probability(key), m[-2], m[-1]]
+	# 	end	
+	# end
 
-		if (m.last != '*') && (m.last != '(XX)') && m.last != ('P/A') && m.last != ('(DQ)')
-			x = [calculate_probability(key), m.last]
-		elsif m.last == '(S)'
-			x = [calculate_probability(key), m.last]
-		elsif move == 'ROPES'
-			x = [calculate_probability(key), move]
-		else
-			x = [calculate_probability(key), m[-2], m[-1]]
-		end	
-	end
+	# # Takes in 2d6/36 value of a roll on an OC card and
+	# # the string value of the points total. It converts
+	# # the String to a float and multiplies the value.
+	# # It then multiplies that number by the probability
+	# # that OC will be rolled in the General Card.
+	# # This returns a points per roll float starting from
+	# # the beginning of a round.
+	# def calculate_oc_ropes_points(array, gc_oc_roll)
+	# 	p = 0
 
-	# Takes in 2d6/36 value of a roll on an OC card and
-	# the string value of the points total. It converts
-	# the String to a float and multiplies the value.
-	# It then multiplies that number by the probability
-	# that OC will be rolled in the General Card.
-	# This returns a points per roll float starting from
-	# the beginning of a round.
-	def calculate_oc_ropes_points(array, gc_oc_roll)
-		p = 0
+	# 	array.each { |x|
+	# 		p += (x[0].to_f * x[1].to_f)
+	# 	}
+	# 	points = p * gc_oc_roll
 
-		array.each { |x|
-			p += (x[0].to_f * x[1].to_f)
-		}
-		points = p * gc_oc_roll
-		
-		return points
-	end
+	# 	return points
+	# end
 
 
-	def calculate_pa_ropes_sub_xx_dq(array)
-		prob = {}
+	# def calculate_pa_ropes_sub_xx_dq(array, gc_oc_roll)
+	# 	prob = {}
 
-		dq = 0.to_f
-		pa = 0.to_f
-		ropes = 0.to_f
-		subm = 0.to_f
-		specialty = 0.to_f
-		xx = 0.to_f
+	# 	dq = 0.to_f
+	# 	pa = 0.to_f
+	# 	ropes = 0.to_f
+	# 	subm = 0.to_f
+	# 	specialty = 0.to_f
+	# 	xx = 0.to_f
 
-		array.each { |a|
-			if a[1] == 'ROPES'
-				ropes += a[0].to_f
-			elsif a[1] == '(S)'
-				specialty += a[0].to_f
-			elsif a[2] != nil
-				case a[2]
-				when '*'
-					subm += a[0].to_f
-				when '(DQ)'
-					dq += a[0].to_f
-				when '(XX)'
-					xx = a[0].to_f
-				when 'P/A'
-					pa += a[0].to_f
-				end
-			end
-		}
-			prob = { :specialty => specialty, :subm => subm, :dq => dq, :xx => xx, :pa => pa, :ropes => ropes }
-	end
+	# 	array.each { |a|
+	# 		if a[1] == 'ROPES'
+	# 			ropes += a[0].to_f
+	# 		elsif a[1] == '(S)'
+	# 			specialty += a[0].to_f
+	# 		elsif a[2] != nil
+	# 			case a[2]
+	# 			when '*'
+	# 				binding.pry
+	# 				subm += a[0].to_f
+	# 			when '(DQ)'
+	# 				dq += a[0].to_f
+	# 			when '(XX)'
+	# 				xx = a[0].to_f
+	# 			when 'P/A'
+	# 				pa += a[0].to_f
+	# 			end
+	# 		end
+	# 	}
+	# 		prob = { :specialty => specialty * gc_oc_roll, :subm => subm * gc_oc_roll, :dq => dq * gc_oc_roll, :xx => xx * gc_oc_roll, :pa => pa * gc_oc_roll, :ropes => ropes * gc_oc_roll }
+	# end
 
 end
