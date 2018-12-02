625a832c269fcc2e1ee85c26dab66d9fcb9276cb
mconiaris <mconiaris@gmail.com>
2018-12-02 09:01:33 -0500

    Modified main program to loop through input file.

--- main.rb
+++ main.rb
@@ -4,47 +4,57 @@ require_relative 'lib/analyzer'
 require_relative 'lib/scraper'
 require_relative 'lib/wrestler'
 
-# puts "Enter in a card location: "
-# pwg_card = STDIN.gets.chomp
-# puts "Looking for #{pwg_card}." 
-# binding.pry
 
-pwg_card = "files/Zak Knight.pdf"
-# Scrape PDF
-# @scrape = Scraper.new("files/Zak Knight.pdf")
-@scrape = Scraper.new(pwg_card)
 
-# Isolate card moves array
-card = @scrape.card
+def scraping(file)
+	# Scrape PDF
+	@scrape = Scraper.new(file)
 
-# Convert move array into a hash and fix formatting.
-moves = @scrape.process_card(card)
+	# Isolate card moves array
+	card = @scrape.card
 
-# Create Wrestler object
-@wrestler = Wrestler.new(moves)
-@analyzer = Analyzer.new
-stats = @analyzer.analyze(@wrestler)
+	# Convert move array into a hash and fix formatting.
+	moves = @scrape.process_card(card)
 
-# Add stats to wrestler instance
-@wrestler.statistics = stats
-puts "Name: #{@wrestler.values[:name]}"
-puts "Card Rating: #{@wrestler.statistics[:card_rating]}"
-puts "OC Probability: #{@wrestler.statistics[:oc_probability]}"
-puts "TT Probability: #{@wrestler.statistics[:tt_probability]}"
-puts "Points-Per-Round: #{@wrestler.statistics[:total_card_points_per_round]}"
-puts "DQ Probability-Per-Round: #{@wrestler.statistics[:dq_probability_per_round]}"
-puts "P/A Probability-Per-Round: #{@wrestler.statistics[:pa_probability_per_round]}"
-puts "Submission Roll Probability-Per-Round: #{@wrestler.statistics[:sub_probability_per_round]}"
-puts "XX Roll Probability-Per-Round: #{@wrestler.statistics[:xx_probability_per_round]}"
-puts "DQ Probability-Per-Round: #{@wrestler.statistics[:dq_probability_per_round]}"
-puts "Submission Loss Probability: #{@wrestler.statistics[:submission_loss_probabilty]}"
-puts "Tag Team Save Probability: #{@wrestler.statistics[:tag_team_save_probabilty]}"
-puts "Singles Priority: #{@wrestler.values[:PriorityS]}"
-puts "Tag Team Priority: #{@wrestler.values[:PriorityT]}"
-puts "Set: #{@wrestler.values[:Set]}"
+	# Create Wrestler object
+	@wrestler = Wrestler.new(moves)
+	@analyzer = Analyzer.new
+	stats = @analyzer.analyze(@wrestler)
+
+	# TODO: Move this printout to Wrestler Object
+	# Add stats to wrestler instance
+	@wrestler.statistics = stats
+	puts "Name: #{@wrestler.values[:name]}"
+	puts "Card Rating: #{@wrestler.statistics[:card_rating]}"
+	puts "OC Probability: #{@wrestler.statistics[:oc_probability]}"
+	puts "TT Probability: #{@wrestler.statistics[:tt_probability]}"
+	puts "Points-Per-Round: #{@wrestler.statistics[:total_card_points_per_round]}"
+	puts "DQ Probability-Per-Round: #{@wrestler.statistics[:dq_probability_per_round]}"
+	puts "P/A Probability-Per-Round: #{@wrestler.statistics[:pa_probability_per_round]}"
+	puts "Submission Roll Probability-Per-Round: #{@wrestler.statistics[:sub_probability_per_round]}"
+	puts "XX Roll Probability-Per-Round: #{@wrestler.statistics[:xx_probability_per_round]}"
+	puts "DQ Probability-Per-Round: #{@wrestler.statistics[:dq_probability_per_round]}"
+	puts "Submission Loss Probability: #{@wrestler.statistics[:submission_loss_probabilty]}"
+	puts "Tag Team Save Probability: #{@wrestler.statistics[:tag_team_save_probabilty]}"
+	puts "Singles Priority: #{@wrestler.values[:PriorityS]}"
+	puts "Tag Team Priority: #{@wrestler.values[:PriorityT]}"
+	puts "Set: #{@wrestler.values[:Set]}"
+end
+
 
 # TODO: Create unit tests for each method
 # TODO: Refactor code to make it clearer. Improve notes.
 # TODO: Revise README.md
 # TODO: Add funcionality to select files and use the program.
+
+pwg_card = "files/Zak Knight.pdf"
+scraping(pwg_card)
+
+# Cycle through input file for PDF files.
+File.open("files/input.txt", "r") do |f|
+  f.each_line do |line|
+    scraping(line.chomp)
+  end  
+end 
+
 
