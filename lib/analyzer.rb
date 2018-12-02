6463909996abd622094329b27b3beeafce1c1e98
mconiaris <mconiaris@gmail.com>
2018-12-02 13:29:28 -0500

    Deleted puts statement. Added a file to gitignore.

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -55,7 +55,7 @@ class Analyzer
 					# puts "#{key}: #{value}"
 					gc_oc_roll_probability += calculate_gc_oc_roll_probability(key, value)
 				elsif k[0..1] == 'DC'
-					puts "#{key}: #{value}"
+					# puts "#{key}: #{value}"
 					dc_points_without_reverse += calculate_dc_points(key, value)
 					# dc_points += analyze_dc(key, value)
 				elsif k[0] == "S" && k[1] != "p" && k[1] != 'e'
