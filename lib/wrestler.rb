f21b1f51dce2772f7ad7fb0c6b71e537b5958a90
mconiaris <mconiaris@gmail.com>
2019-01-09 18:11:23 -0500

    Moved code that prints to csv from main program to Wrestler class.

--- lib/wrestler.rb
+++ lib/wrestler.rb
@@ -7,5 +7,27 @@ class Wrestler
 		@values = card
 	end
 
-	
+	# Takes Wrestler object statistics and prints them
+	# out to a CSV file.
+	def wrestler_output(wrestler)
+		puts "Name: #{wrestler.values[:name]}"
+		puts "Set: #{wrestler.values[:Set]}"
+		puts "Singles Priority: #{wrestler.values[:PriorityS]}"
+		puts "Tag Team Priority: #{wrestler.values[:PriorityT]}"
+		puts "TT Probability: #{wrestler.statistics[:tt_probability]}"
+		puts "Card Rating: #{wrestler.statistics[:card_rating]}"
+		puts "OC Probability: #{wrestler.statistics[:oc_probability]}"
+		puts "Total Points-Per-Round: #{wrestler.statistics[:total_card_points_per_round]}"
+		puts "DQ Probability-Per-Round: #{wrestler.statistics[:dq_probability_per_round]}"
+		puts "P/A Probability-Per-Round: #{wrestler.statistics[:pa_probability_per_round]}"
+		puts "Submission Roll Probability-Per-Round: #{wrestler.statistics[:sub_probability_per_round]}"
+		puts "XX Roll Probability-Per-Round: #{wrestler.statistics[:xx_probability_per_round]}"
+		puts "Submission Loss Probability: #{wrestler.statistics[:submission_loss_probabilty]}"
+		puts "Tag Team Save Probability: #{wrestler.statistics[:tag_team_save_probabilty]}"
+		puts "\n"
+
+		f = File.new('files/results.csv', 'a')
+		f.write("#{wrestler.values[:name]},#{wrestler.values[:Set]}, #{wrestler.values[:PriorityS]}, #{wrestler.values[:PriorityT]}, #{wrestler.statistics[:tt_probability]}, #{wrestler.statistics[:card_rating]}, #{wrestler.statistics[:oc_probability]}, #{wrestler.statistics[:total_card_points_per_round]}, #{wrestler.statistics[:dq_probability_per_round]}, #{wrestler.statistics[:pa_probability_per_round]}, #{wrestler.statistics[:sub_probability_per_round]}, #{wrestler.statistics[:xx_probability_per_round]}, #{wrestler.statistics[:submission_loss_probabilty]}, #{wrestler.statistics[:tag_team_save_probabilty]}, \n")
+		f.close
+	end
 end
--- main.rb
+++ main.rb
@@ -24,7 +24,7 @@ def scraping(file)
 	# TODO: Move this printout to Wrestler Object
 	# Add stats to wrestler instance
 	@wrestler.statistics = stats
-	wrestler_output(@wrestler)
+	@wrestler.wrestler_output(@wrestler)
 end
 
 
@@ -46,29 +46,7 @@ def scraping_converted(file)
 
 	# Add stats to wrestler instance
 	@wrestler.statistics = stats
-	wrestler_output(@wrestler)
-end
-
-def wrestler_output(wrestler)
-	puts "Name: #{@wrestler.values[:name]}"
-	puts "Set: #{@wrestler.values[:Set]}"
-	puts "Singles Priority: #{@wrestler.values[:PriorityS]}"
-	puts "Tag Team Priority: #{@wrestler.values[:PriorityT]}"
-	puts "TT Probability: #{@wrestler.statistics[:tt_probability]}"
-	puts "Card Rating: #{@wrestler.statistics[:card_rating]}"
-	puts "OC Probability: #{@wrestler.statistics[:oc_probability]}"
-	puts "Total Points-Per-Round: #{@wrestler.statistics[:total_card_points_per_round]}"
-	puts "DQ Probability-Per-Round: #{@wrestler.statistics[:dq_probability_per_round]}"
-	puts "P/A Probability-Per-Round: #{@wrestler.statistics[:pa_probability_per_round]}"
-	puts "Submission Roll Probability-Per-Round: #{@wrestler.statistics[:sub_probability_per_round]}"
-	puts "XX Roll Probability-Per-Round: #{@wrestler.statistics[:xx_probability_per_round]}"
-	puts "Submission Loss Probability: #{@wrestler.statistics[:submission_loss_probabilty]}"
-	puts "Tag Team Save Probability: #{@wrestler.statistics[:tag_team_save_probabilty]}"
-	puts "\n"
-
-	f = File.new('files/results.csv', 'a')
-	f.write("#{@wrestler.values[:name]},#{@wrestler.values[:Set]}, #{@wrestler.values[:PriorityS]}, #{@wrestler.values[:PriorityT]}, #{@wrestler.statistics[:tt_probability]}, #{@wrestler.statistics[:card_rating]}, #{@wrestler.statistics[:oc_probability]}, #{@wrestler.statistics[:total_card_points_per_round]}, #{@wrestler.statistics[:dq_probability_per_round]}, #{@wrestler.statistics[:pa_probability_per_round]}, #{@wrestler.statistics[:sub_probability_per_round]}, #{@wrestler.statistics[:xx_probability_per_round]}, #{@wrestler.statistics[:submission_loss_probabilty]}, #{@wrestler.statistics[:tag_team_save_probabilty]}, \n")
-	f.close
+	@wrestler.wrestler_output(@wrestler)
 end
 
 
