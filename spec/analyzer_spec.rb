19024ef185c493d038f31283ceefea0d4118bb8a
mconiaris <mconiaris@gmail.com>
2019-01-22 15:45:29 -0500

    Added rspect test for analyzer_spec.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -51,6 +51,9 @@ RSpec.describe Analyzer do
 			it 'should return 1/36.to_r when \'OC02\' is passed to it' do
 				expect(@analyze.calculate_probability('OC02')).to eq(1/36.to_r)
 			end
+			it 'should return 2/36.to_r when \'OC03\' is passed to it' do
+				expect(@analyze.calculate_probability('OC03')).to eq(2/36.to_r)
+			end
 		end
 	end
 
