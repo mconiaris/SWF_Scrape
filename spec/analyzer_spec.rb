43369e0eef772a04fcf79d7146325b253258e25a
mconiaris <mconiaris@gmail.com>
2019-01-30 10:56:29 -0500

    Revised rspec tests.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -9,44 +9,22 @@ RSpec.describe Analyzer do
 		it "should create an empty @statistics hash" do
 			expect(analyze.statistics).to be_instance_of(Hash)
 		end
-
+		it "should create a variable that will respond_to #analyze_gc" do
+			expect(analyze.respond_to?(:analyze_gc))
+		end
+		it "should create a variable that will respond_to analyze_dc" do
+			expect(analyze.respond_to?(:analyze_dc))
+		end
+		it "should create a variable that will respond_to analyze_s" do
+			expect(analyze.respond_to?(:analyze_s))
+		end
+		it "should create a variable that will respond_to submission_probability" do
+			expect(analyze.respond_to?(:submission_probability))
+		end
+		it "should create a variable that will respond_to offense_points" do
+			expect(analyze.respond_to?(:offense_points))
+		end
 	end	
-
-		describe '#analyze_gc' do
-			it "should respond_to analyze_gc" do
-				expect(analyze.respond_to?(:analyze_gc))
-			end
-		end
-
-		describe '#analyze_dc' do
-			it "should respond_to analyze_dc" do
-				expect(analyze.respond_to?(:analyze_dc))
-			end
-		end
-
-		describe '#analyze_s' do
-			it "should respond_to analyze_s" do
-				expect(analyze.respond_to?(:analyze_s))
-			end
-		end
-
-		describe '#submission_probability' do
-			it "should respond_to submission_probability" do
-				expect(analyze.respond_to?(:submission_probability))
-			end
-		end
-
-		describe '#offense_points' do
-			it "should respond_to offense_points" do
-				expect(analyze.respond_to?(:offense_points))
-			end
-		end
-
-		describe '#analyze @statistics' do
-			it 'should return a hash' do
-				expect(analyze.statistics.class).instance_of?(Hash)
-			end
-		end
 
 		describe '#calculate_probility' do
 			it 'should return 1/36.to_r when \'OC02\' is passed to it' do
