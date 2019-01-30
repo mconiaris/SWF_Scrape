fc306d673250f40df19982a0779f6eddbec3ef78
mconiaris <mconiaris@gmail.com>
2019-01-30 11:24:44 -0500

    Added context block to Analyze rspec test.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -6,25 +6,27 @@ RSpec.describe Analyzer do
 	let(:analyze) { Analyzer.new }
 
 	describe '#initialize' do
-		it "should create an empty @statistics hash" do
-			expect(analyze.statistics).to be_instance_of(Hash)
+			context 'after initialization' do
+			it "should create an empty @statistics hash" do
+				expect(analyze.statistics).to be_instance_of(Hash)
+			end
+			it "should create a variable that will respond_to #analyze_gc" do
+				expect(analyze.respond_to?(:analyze_gc))
+			end
+			it "should create a variable that will respond_to analyze_dc" do
+				expect(analyze.respond_to?(:analyze_dc))
+			end
+			it "should create a variable that will respond_to analyze_s" do
+				expect(analyze.respond_to?(:analyze_s))
+			end
+			it "should create a variable that will respond_to submission_probability" do
+				expect(analyze.respond_to?(:submission_probability))
+			end
+			it "should create a variable that will respond_to offense_points" do
+				expect(analyze.respond_to?(:offense_points))
+			end
 		end
-		it "should create a variable that will respond_to #analyze_gc" do
-			expect(analyze.respond_to?(:analyze_gc))
-		end
-		it "should create a variable that will respond_to analyze_dc" do
-			expect(analyze.respond_to?(:analyze_dc))
-		end
-		it "should create a variable that will respond_to analyze_s" do
-			expect(analyze.respond_to?(:analyze_s))
-		end
-		it "should create a variable that will respond_to submission_probability" do
-			expect(analyze.respond_to?(:submission_probability))
-		end
-		it "should create a variable that will respond_to offense_points" do
-			expect(analyze.respond_to?(:offense_points))
-		end
-	end	
+	end
 
 	describe '#calculate_probility' do
 		it 'should return 1/36.to_r when \'OC02\' is passed to it' do
