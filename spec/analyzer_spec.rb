32855ef9abc2a7f3c8493c66ed7d634401b8c52f
mconiaris <mconiaris@gmail.com>
2019-02-07 10:54:54 -0500

    Added frame for calculate_reverse_roll_probability(wrestler_hash, gc_dc_roll_probability) method rspec test.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -240,6 +240,10 @@ RSpec.describe Analyzer do
 		end
 	end
 
+	# calculate_reverse_roll_probability(wrestler_hash, gc_dc_roll_probability)
+	describe '#calculate_reverse_roll_probability' do
+	end
+
 
 	# TODO: Make tests to confirm math is right.
 	# TODO: Create test that deals with DQs in Specialty moves.
