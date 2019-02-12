d14aac3392f16530f5f0a780c35368c9ebe77a5c
mconiaris <mconiaris@gmail.com>
2019-02-12 10:19:54 -0500

    Added another calculate_reverse_roll_probability rspec test.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -353,6 +353,13 @@ RSpec.describe Analyzer do
 
 	# calculate_reverse_roll_probability(wrestler_hash, gc_dc_roll_probability)
 	describe '#calculate_reverse_roll_probability' do
+		context 'when a \':GC06 value of \'REVERSE\' and a DC roll probability of \'21/36.to_r\' is passed' do
+			it 'should return (35/432)' do
+				dc_hash = { :DC02 => 'B', :DC03 => 'B', :DC04 => 'A', :DC05 => 'B', :DC06 => 'REVERSE' }
+				dc_roll_prob = 21/36.to_r
+				expect(analyze.calculate_reverse_roll_probability(dc_hash, dc_roll_prob)).to eq(35/432.to_r)
+			end
+		end
 		context 'when a \':GC07 value of \'REVERSE\' and a DC roll probability of \'21/36.to_r\' is passed' do
 			it 'should return (7/72)' do
 				dc_hash = { :DC02 => 'B', :DC03 => 'B', :DC04 => 'A', :DC05 => 'B', :DC06 => 'C', :DC07 => 'REVERSE' }
