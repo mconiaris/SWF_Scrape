944a9bca2497fb2946ba49cc4246d24c46411840
mconiaris <mconiaris@gmail.com>
2019-02-21 14:32:26 -0500

    Added rspec test for calculate_dc_points method.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -523,11 +523,11 @@ RSpec.describe Analyzer do
 				expect(analyze.calculate_dc_points(dc_key, dc_value)).to eq(0.16666666666666666)
 			end
 		end
-		xcontext 'when a hash with a key of \':DC11\' and a value of \'A\' is passed' do
+		context 'when a hash with a key of \':DC11\' and a value of \'A\' is passed' do
 			dc_key = :DC11
 			dc_value = 'A'
-			it 'returns 0' do
-				expect(analyze.calculate_dc_points(dc_key, dc_value)).to eq(0)
+			it 'returns 0.1111111111111111' do
+				expect(analyze.calculate_dc_points(dc_key, dc_value)).to eq(0.1111111111111111)
 			end
 		end
 		xcontext 'when a hash with a key of \':DC12\' and a value of \'A\' is passed' do
