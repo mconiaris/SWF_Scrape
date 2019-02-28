e875d124d4b7d92aa1d38acb500f2b1bbc8bf949
mconiaris <mconiaris@gmail.com>
2019-02-28 15:06:02 -0500

    Refactored calculate_dc_points test.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -654,8 +654,8 @@ RSpec.describe Analyzer do
 		context 'when a hash with a key of \':DC07\' and a value of \'C\' is passed' do
 			dc_key = :DC07
 			dc_value = 'C'
-			it 'returns 0.6666666666666666' do
-				expect(analyze.calculate_dc_points(dc_key, dc_value)).to eq(0.6666666666666666)
+			it 'returns a value within 0.001 of 0.666' do
+				expect(analyze.calculate_dc_points(dc_key, dc_value)).to be_within(0.001).of(0.666)
 			end
 		end
 		context 'when a hash with a key of \':DC08\' and a value of \'C\' is passed' do
