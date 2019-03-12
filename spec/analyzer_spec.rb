0d56c90ed54f875f73477edf9263523653fa2cb3
mconiaris <mconiaris@gmail.com>
2019-03-11 20:13:15 -0400

    Created rspec test for calculate_specialty_dq_pa_subm_xx_probability.

--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -894,6 +894,12 @@ RSpec.describe Analyzer do
 				expect(analyze.calculate_specialty_dq_pa_subm_xx_probability(w, 'XX')).to eq({:OC=>0, :R=>4/36.to_r })
 			end
 		end
+		context 'when a wrestler object includes { :OC07=>"XX", :OC08=>"XX" } and the move attribute is \'XX\'' do
+			it 'returns a value of {:OC=>11/36, :R=>0 }' do
+				w = { :OC07=>"XX", :OC08=>"XX" }
+				expect(analyze.calculate_specialty_dq_pa_subm_xx_probability(w, 'XX')).to eq({:OC=>11/36.to_r, :R=>0 })
+			end
+		end
 	end
 	
 
