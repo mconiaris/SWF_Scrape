a1186e905c41185c164a3a55bdb0578e57e23981
mconiaris <mconiaris@gmail.com>
2019-03-21 13:59:46 -0400

    Created test for Scraper's process_card method.

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -8,8 +8,6 @@ RSpec.describe Scraper do
 			it "should have one page" do
 				expect(@scrape.reader.page_count).to eq(1)
 			end
-			describe '#reader' do
-			end
 			it "should have 1469 characters" do
 				expect(@scrape.reader.page(1).text.size).to eq(1469)
 			end
@@ -20,6 +18,7 @@ RSpec.describe Scraper do
 				expect(@scrape.card[0].include?('Zak Knight')).to eq(true)
 			end
 		end
+
 		describe 'Scraper#process_card' do
 			it 'responds to #process_card method' do
 				expect(@scrape.respond_to?(:process_card)).to eq(true)
@@ -28,9 +27,13 @@ RSpec.describe Scraper do
 				card = @scrape.card
 				expect(@scrape.process_card(card)).to be_kind_of(Hash)
 			end
+			it 'includes a :name property that is \'Zak Knight\'' do
+				card = @scrape.card
+				result = @scrape.process_card(card)
+				expect(result[:name]).to eq("Zak Knight")
+			end
 		end
 	end
 	# TODO: Add tests to confirm hash that creates Wrestler object
-	# TODO: Expect Hash-name to eq: Zak Knight
 end
 
