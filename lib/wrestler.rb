e6cf8bd776cf3a44d236d72a9a4d47ffbe6c2efc
mconiaris <mconiaris@gmail.com>
2020-01-18 12:24:48 -0500

    Changed location of output files.

--- lib/wrestler.rb
+++ lib/wrestler.rb
@@ -26,13 +26,13 @@ class Wrestler
 		puts "Tag Team Save Probability: #{wrestler.statistics[:tag_team_save_probabilty]}"
 		puts "\n"
 
-		f = File.new('files/results.csv', 'a')
+		f = File.new('files/output/results.csv', 'a')
 		f.write("#{wrestler.values[:name]},#{wrestler.values[:Set]}, #{wrestler.values[:PriorityS]}, #{wrestler.values[:PriorityT]}, #{wrestler.statistics[:tt_probability]}, #{wrestler.statistics[:card_rating]}, #{wrestler.statistics[:oc_probability]}, #{wrestler.statistics[:total_card_points_per_round]}, #{wrestler.statistics[:dq_probability_per_round]}, #{wrestler.statistics[:pa_probability_per_round]}, #{wrestler.statistics[:sub_probability_per_round]}, #{wrestler.statistics[:xx_probability_per_round]}, #{wrestler.statistics[:submission_loss_probabilty]}, #{wrestler.statistics[:tag_team_save_probabilty]}, \n")
 		f.close
 	end
 
 	def wrestler_values_output(wrestler)
-		f = File.new("files/#{wrestler.values[:name]}_values.csv", 'a')
+		f = File.new("files/output/#{wrestler.values[:name]}_values.csv", 'a')
 		wrestler.values.each { |k, v|
 			f.write(k, ",", v, "\n")  }
 		f.write("Sub:, #{wrestler.values[:Sub][0]}-#{wrestler.values[:Sub][1]}\n")
@@ -42,7 +42,7 @@ class Wrestler
 
 	def wrestler_points_values(wrestler)
 		binding.pry
-		f = File.new("files/#{wrestler.values[:name]}_points_values.csv", 'a')
+		f = File.new("files/output/#{wrestler.values[:name]}_points_values.csv", 'a')
 		f.write("Sub:, #{wrestler.values[:Sub][0]}-#{wrestler.values[:Sub][1]}\n")
 		f.write("Tag:, #{wrestler.values[:Tag][0]}-#{wrestler.values[:Tag][1]}\n")
 		f.write("Priority:, #{wrestler.values[:PriorityS]}/#{wrestler.values[:PriorityT]}\n")
