3522f33beb739a4a40314195d03ea478696735d8
mconiaris <mconiaris@gmail.com>
2020-02-19 12:10:37 -0500

    Installed henkei to get data from a Pages document.

--- lib/pages_scraper.rb
+++ lib/pages_scraper.rb
@@ -1,3 +1,10 @@
+require 'henkei'
+
 class PagesScraper
+
+	data = File.read 'sample.pages'
+	text = Henkei.read :text, data
+	metadata = Henkei.read :metadata, data
+	mimetype = Henkei.read :mimetype, data
 
 end
